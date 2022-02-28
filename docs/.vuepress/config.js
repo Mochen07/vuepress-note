da79674c6390f6df81479fa2edaf5f7c7671805d
mochen <mochen7777@gmail.com>
2022-02-28 18:23:22 +0800

    定义核心思想 临摹~

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // 站点配置
   lang: 'zh-CN',
-  title: '你好，VuePress ! ',
-  description: '这是我的第一个 VuePress 站点',
+  title: '滴水',
+  description: '积土为山积水为海。',
 
   // 主题和它的配置
   theme: '@vuepress/theme-default',
