00d93faae22b78b7bbb5e3f93e7c88ef5da388a1
mochen <mochen7777@gmail.com>
2022-03-03 15:59:17 +0800

    那些年走过的弯路，历历在目。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  // 站点配置
-  lang: 'zh-CN',
-  title: '滴水',
-  description: '积土为山积水为海。',
-
-  // 主题和它的配置
-  theme: '@vuepress/theme-default',
-  themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
-  }
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  evergreen: false, // 浏览器兼容性
+  plugins: [
+    // ['@vuepress/search', {searchMaxSuggestions: 10}]
+  ]
 }
